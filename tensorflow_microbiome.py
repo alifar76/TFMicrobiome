8d01fa0c10073c0620c143c109bd4a92100cc90a
Ali Ahmad Faruqi <alifaruqi@Alis-MacBook-Pro.local>
2016-06-21 23:03:26 -0700

    fixed code for tensorflow. Accuracy:0.764

--- tensorflow_microbiome.py
+++ tensorflow_microbiome.py
@@ -27,9 +27,58 @@ v = ['HIV_postive' if x == 'True' else 'HIV_negative' if x == 'False' else 'Unde
 b.loc[:,'HIV status'] = pd.Series(v, index=b.index)
 c = b[b['HIV status'].isin(['HIV_postive', 'HIV_negative'])]
 # No. of samples to train/test the model
-n_train = math.ceil(train_ratio*c.shape[0])
-n_test = c.shape[0] - n_train
+n_train = int(math.ceil(train_ratio*c.shape[0]))
 
-for index, row in c.iterrows():
-  print row.drop('HIV status', axis=0).values
-  #print row
+train_dataset = pd.DataFrame()
+test_dataset = pd.DataFrame()
+train_dataset = c[:n_train]
+test_dataset = c[n_train:]
+
+
+test_input = []
+test_output = []
+for index, row in test_dataset.iterrows():
+  # Store 0th-index is HIV postive and 1st-index is HIV negative
+  store = [0,0]
+  otudat = row.drop('HIV status', axis=0).values
+  if row['HIV status'] == 'HIV_postive':
+    store[0] = 1
+  else:
+    store[1] = 1
+  test_input.append(otudat)
+  test_output.append(store)
+
+#print train_dataset.shape, test_dataset.shape
+
+
+
+###### TF code
+x = tf.placeholder(tf.float32, [None, 267])
+W = tf.Variable(tf.zeros([267, 2]))
+b = tf.Variable(tf.zeros([2]))
+
+y = tf.nn.softmax(tf.matmul(x, W) + b)
+y_ = tf.placeholder(tf.float32, [None, 2])
+
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
+train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+init = tf.initialize_all_variables()
+sess = tf.Session()
+sess.run(init)
+for index, row in train_dataset.iterrows():
+  # Store 0th-index is HIV postive and 1st-index is HIV negative
+  store = [0,0]
+  otudat = row.drop('HIV status', axis=0).values
+  if row['HIV status'] == 'HIV_postive':
+    store[0] = 1
+  else:
+    store[1] = 1
+  response = np.asarray(store)
+  batch_xs = np.reshape(otudat, (-1, 267))
+  batch_ys = np.reshape(response, (-1, 2))
+  sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
+
+
+correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+print(sess.run(accuracy, feed_dict={x: np.asarray(test_input), y_: np.asarray(test_output)}))
